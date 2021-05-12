2877c0dceea6473349a46e6fcd6de0535cd3e1f7
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-12 17:23:02 +0530

    Added:class GeometryUC1.java

---
+++ BridgeLabz/src/day3pdf2/GeometryUC1.java
@@ -0,0 +1,17 @@
+package day3pdf2;
+
+public class GeometryUC1 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		 int x1,x2,y1,y2;
+		    double dis;
+		    x1=1;y1=1;x2=4;y2=4;
+		    dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+	            System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+		}
+	}
+
+
